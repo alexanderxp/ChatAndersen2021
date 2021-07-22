8d812b238020ce1b4ddd5426469ab06451498629
alexanderxp <alexanderxp@ukr.net>
2021-07-22 15:28:24 +0300

    add void function & document.querySelector &  blurVal = 0;  in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,14 @@ import './../sass/styles.scss';
 //import 'font-awesome/sass/font-awesome.min.scss';
 //import { threadId } from 'worker_threads';
 
+void function () {
+  let blurVal = 15, LOGIN = false, currentUserChatId = 0, messages = [], users = [], loggedUser = '';
+
+  document.addEventListener( "DOMContentLoaded", function() {
+
+  document.querySelector('body > div.login-modal').style.display = "none";   // для отсутствия окна "логина и пароля"
+  blurVal = 0;                                                               // закрытие чата размытием
+
+});
+
+}();
