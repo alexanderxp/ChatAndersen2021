88648391781c1a9ec08b7212ea32e03641102ff4
alexanderxp <alexanderxp@ukr.net>
2021-07-22 15:19:11 +0300

    del mediaindo titles in index.html & add index.js

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import 'babel-polyfill';
+import _ from 'lodash';
+
+import './../sass/styles.scss';
+//import 'font-awesome/sass/font-awesome.min.scss';
+//import { threadId } from 'worker_threads';
+
